3220c048c9cd97596092cfc2e72f62817b255ae3
tadhg-moore <tadhgm@vt.edu>
2021-03-21 10:41:44 -0400

    delete extra code

--- pdf_parsing.r
+++ pdf_parsing.r
@@ -1,26 +1,58 @@
 # Necessary packages 
 library(tidyverse)
 library(pdftools)
-library(leaflet)
-library(tm)
-
-# testing leaflet maps
-m <- leaflet() %>%
-  addTiles() %>%  # Add default OpenStreetMap map tiles
-  addMarkers(lng=174.768, lat=-36.852, popup="The birthplace of R")
-m  # Print the map
+library(stringi)
 
 # Creating a vector of PDF files names using the list.files function. The pattern argument says to only grab those files ending with "pdf"
-files <- list.files(pattern = "pdf$")
-files
+# Extract direct from URLS
+urls <- c("https://www.npws.ie/sites/default/files/publications/pdf/Perrin_et_al_2008_NSNW_V3a.pdf")
 
-opinions <- lapply(files, pdf_text)
-# opinions
+opinions <- lapply(urls, pdf_text)
+pages <- length(opinions[[1]])
 
-lapply(opinions, length)
+# loop through pages & exrtact info
+ls_out <- lapply(2:pages, function(x) {
+  
+  # x <- 2 # Use this to test
+  
+  plines <- strsplit(opinions[[1]][x], "\\n")[[1]]
+  
+  # Extract site id
+  site_line <- grep("Site no.", plines)
+  site_id <- stri_extract_first_regex(plines[site_line], "[0-9]+")
+  
+  # Extract woodland name
+  wl_line <- grep("Woodland name", plines)
+  wl <- str_squish(plines[wl_line])
+  woodland_name <- gsub('^.*Woodland name\\s*|\\s*Townland.*$', '', wl)
+  
+  # Extract Area
+  area_line <- grep("Area", plines)
+  area <- as.numeric(stri_extract_first_regex(plines[area_line], "[0-9]+"))
+  
+  # conservation & threat
+  cons_line <- grep("Conservation", plines)
+  cl <- str_squish(plines[cons_line])
+  scores <- as.numeric(stri_extract_all(cl, regex = "[0-9]+")[[1]])
+  cons_rate <- gsub('^.*Conservation rating and score\\s*|\\s*[0-9]+.*$', '', cl)
+  cons_score <- scores[1]
+  threat_score <- scores[2]
+  threat_rate <- gsub('^.*Threat rating and score\\s*|\\s*[0-9]+.*$', '', cl)
+  df <- data.frame(site_id, woodland_name, area, cons_rate, cons_score, threat_rate, threat_score)
+  
+  return(df[1, ])
+  
+})
 
-opinions[1] # first pdf 
+# Bind all elements in the list by rows to make a dataframe
+df <- do.call(rbind, ls_out)
+df
+str(df)
 
-# Set up text mining package to extract data from these pdf files now in R
+# simple plot
+ggplot(df) +
+  geom_point(aes(area, cons_score, shape = cons_rate)) +
+  facet_wrap(~threat_rate) +
+  scale_x_log10()
 
 
