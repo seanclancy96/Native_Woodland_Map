c476d69f0f88af1b9d0b4a038208fcfdae52915d
John <johnmullane10@gmail.com>
2021-04-01 22:02:22 +0100

    Updated button functionality

--- app.R
+++ app.R
@@ -25,19 +25,40 @@ df_clean %>% filter(woodland_name %in% johns_name) # Checking new data frame con
 levels(df_clean$ownership)[levels(df_clean$ownership)==""] <- "Not Stated"
 
 # Taking non-private woods only
-df_clean_non_private <- df_clean %>% 
-  filter(!grepl("Private", ownership)) 
+# df_clean_non_private <- df_clean %>% 
+#   filter(!grepl("Private", ownership)) 
+
+# Testing combining filter functions
+# df_clean_pub <- df_clean %>% 
+#  filter(!grepl("Private", ownership) & !ownership %in% "Not Stated") 
+
+# Set a new variable which denotes if it is a public or private wood
+pub_priv <- c("a")
+
+df_clean2 <- df_clean %>%
+  add_column(pub_priv) 
+
+# Using a loop to set a pub/priv indicator for map
+for (i in 1:nrow(df_clean2)){
+  if (!grepl("Private", df_clean2$ownership[i]) & !df_clean2$ownership[i] %in% "Not Stated"){
+    df_clean2$pub_priv[i] <- "Public"
+  }
+  else {
+    df_clean2$pub_priv[i] <- "Private"
+  }
+}
 
 # Taking definite non-private woods
-df_clean_public_confirmed <- df_clean_non_private %>% 
-  filter(!ownership %in% "Not Stated")
+# df_clean_public_confirmed <- df_clean_non_private %>% 
+# filter(!ownership %in% "Not Stated")
 
 ui <- fluidPage(
   navbarPage("NPWS Forests",
              tabPanel("Map",
                       textOutput("text"),
-                      selectInput("county", label = "County", choices = unique(df_clean$county)),
+                      selectInput("county", label = "County", choices = unique(df_clean2$county)),
                       radioButtons("type", label = "Map type", choices = c("Public", "Private")),
+                      #radioButtons("type", label = "Map type", choices = unique(df_clean2$pub_priv)),
                       leafletOutput("map")
                       )
   )
@@ -50,7 +71,9 @@ server <- function(input, output, session) {
     "John is cool"
   })
   
-  output$map <- renderLeaflet({leaflet(df_clean_public_confirmed %>% filter(county %in% input$county)) %>%
+  output$map <- renderLeaflet({leaflet(df_clean2 %>% 
+                                         filter(county %in% input$county) %>%  
+                                         filter(pub_priv %in% input$type))  %>% # filtering by user input of county
       addTiles() %>%  # Add default OpenStreetMap map tiles
       addMarkers(~lon, ~lat, label = ~htmlEscape(woodland_name), 
                  popup = ~paste0("<font size=3>", '<strong>', woodland_name, '</strong>',
@@ -63,4 +86,5 @@ server <- function(input, output, session) {
 }
 
 shinyApp(ui, server)
+
 
