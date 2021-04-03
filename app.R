6b0890d6ffe9d900edbf87763ea24313300a22bb
seanclancy96 <seanpc1096@gmail.com>
2021-04-03 20:58:05 +0100

    ownership filter working

--- app.R
+++ app.R
@@ -22,7 +22,14 @@ df_clean <- df %>%
 df_clean %>% filter(woodland_name %in% johns_name) # Checking new data frame contains wood of interest
 
 # Changing empty ownership names to Not Stated
-levels(df_clean$ownership)[levels(df_clean$ownership)==""] <- "Not Stated"
+df_clean$ownership[df_clean$ownership==""] <- "Not Stated"
+
+unique(df_clean["ownership"])
+
+df_clean$ownership0 <- ifelse(grepl("private",df_clean$ownership,ignore.case=T),"Private",
+                              ifelse(grepl("coillte|other state body|local authority|Bórd na Móna", df_clean$ownership,ignore.case=T),"Public",
+                              ifelse(grepl("npws",df_clean$ownership,ignore.case=T),"NPWS","Unknown")))
+
 
 # Taking non-private woods only
 df_clean_non_private <- df_clean %>% 
@@ -36,8 +43,8 @@ ui <- fluidPage(
   navbarPage("NPWS Forests",
              tabPanel("Map",
                       textOutput("text"),
-                      selectInput("county", label = "County", choices = unique(df_clean$county)),
-                      radioButtons("type", label = "Map type", choices = c("Public", "Private")),
+                      selectInput("county", label = "County", choices = c("State",unique(df_clean$county))),
+                      radioButtons("owner", label = "Ownership", choices = c("All",unique(df_clean$ownership0))),
                       leafletOutput("map")
                       )
   )
@@ -50,7 +57,26 @@ server <- function(input, output, session) {
     "John is cool"
   })
   
-  output$map <- renderLeaflet({leaflet(df_clean_public_confirmed %>% filter(county %in% input$county)) %>%
+  filtered <- reactive({
+    if(input$county=="State" | input$owner =="All" ){
+      m <-df_clean
+    } else if (input$county!="State" | input$county=="All"){
+      m <-df_clean %>% filter(county %in% input$county)
+    }
+    else if (input$county=="State" | input$county!="All"){
+      m <-df_clean %>% filter(ownership0 %in% input$owner)
+    }
+    else {
+      m <- (df_clean %>% filter(county %in% input$county)) %>% filter(ownership0 %in% input$owner)
+    }
+    m
+    
+  })
+    
+  
+  output$map <- renderLeaflet({
+    
+    leaflet(filtered) %>%
       addTiles() %>%  # Add default OpenStreetMap map tiles
       addMarkers(~lon, ~lat, label = ~htmlEscape(woodland_name), 
                  popup = ~paste0("<font size=3>", '<strong>', woodland_name, '</strong>',
