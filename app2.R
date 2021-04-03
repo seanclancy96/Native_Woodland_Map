14cf7895d281edf437660d9797c34a63304b3737
John <johnmullane10@gmail.com>
2021-04-03 23:28:50 +0100

    Added provinces to input menu

--- app2.R
+++ app2.R
@@ -10,7 +10,7 @@ johns_name <- "St John's Wood"
 lat_johns <- 53.556947
 lon_johns <- -8.00471653
 
-# Changing details for St. John's wood and Phoenix park wood
+# Changing details for some specific woods (St. John's wood, Phoenix park wood and more)
 johns <- df %>% 
   filter(woodland_name %in% johns_name) %>% 
   mutate(lat = lat_johns, lon = lon_johns) 
@@ -19,11 +19,23 @@ borough <- df %>%
   filter(county %in% "Borough") %>% 
   mutate(county = "Dublin")
 
+meath_louth <- df %>% 
+  filter(county %in% "Meath/Louth") %>% 
+  mutate(county = "Meath")
+
+cork_waterford <- df %>% 
+  filter(county %in% "Cork/Waterford") %>% 
+  mutate(county = "Cork")
+
 df_clean <- df %>% 
   filter(!woodland_name %in% johns_name) %>% 
-  add_row(johns) %>% 
   filter(!county %in% "Borough") %>% 
-  add_row(borough)
+  filter(!county %in% "Meath/Louth") %>% 
+  filter(!county %in% "Cork/Waterford") %>% 
+  add_row(johns)  %>% 
+  add_row(borough) %>% 
+  add_row(meath_louth) %>% 
+  add_row(cork_waterford)
 
 # Checking new data frame contains wood of interest
 df_clean %>% 
@@ -37,6 +49,14 @@ df_clean$ownership0 <- ifelse(grepl("private", df_clean$ownership, ignore.case=T
                               ifelse(grepl("coillte|other state body|local authority|Bórd na Móna", df_clean$ownership,ignore.case=T), "Public",
                                      ifelse(grepl("npws", df_clean$ownership, ignore.case=T), "NPWS", "Unknown")))
 
+# Setting provinces counties
+munster <- c("Kerry", "Limerick", "Cork", "Tipperary", "Waterford", "Clare")
+connacht <- c("Galway", "Leitrim", "Mayo", "Roscommon", "Sligo")
+leinster <- c("Kildare", "Carlow", "Kilkenny", "Laois", "Longford", "Louth", 
+              "Meath", "Offaly", "Wexford", "Wicklow", "Westmeath", "Dublin")
+ulster <- c("Monaghan", "Cavan", "Donegal")
+
+
 ui <- bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
   
@@ -45,7 +65,9 @@ ui <- bootstrapPage(
   
   absolutePanel(top = 10, right = 10,
                 radioButtons("owner", label = "Ownership", choices = c("All", "Public", "NPWS")),
-                selectInput("county", label = "County", choices = c("All Counties", unique(df_clean$county))))
+                selectInput("county", label = "County", choices = c("All Counties", unique(df_clean$county), 
+                                                                    "Munster", "Leinster", "Connacht", "Ulster"
+                                                                    )))
 )
 
 
@@ -53,6 +75,10 @@ server <- function(input, output, session) {
 
   output$map <- renderLeaflet({leaflet(df_clean %>% 
                                          filter(if (input$county %in% "All Counties") county %in% county 
+                                                else if (input$county %in% "Munster") county %in% munster
+                                                else if (input$county %in% "Leinster") county %in% leinster
+                                                else if (input$county %in% "Connacht") county %in% connacht
+                                                else if (input$county %in% "Ulster") county %in% ulster
                                                 else county %in% input$county) %>% 
                                          filter(if (input$owner %in% "All") ownership0 %in% ownership0
                                                 else if (input$owner %in% "Public") ownership0 %in% c("Public", "NPWS")
