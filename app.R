e62b347dc9baf22ed3890237975bdecbdaa85ab3
seanclancy96 <seanpc1096@gmail.com>
2021-04-04 12:13:01 +0100

    Manual edit

--- app.R
+++ app.R
@@ -4,32 +4,60 @@ library(leaflet)
 library(htmltools)
 # Reading data 
 df <- readRDS(file = "df.rds")
+wlist=c("Tankardstown South","Ellis Wood")
+df[df$woodland_name==wlist,]
 
-# Setting St. Johns Wood Latitude and Longitude to correct coordinates
-names <- c("St John's Wood","Ellis Wood")
-lats <- c(53.556947,53.551731081502)
-longs <- c(-8.00471653,-9.9475836753845)
 
-# Changing details for St. John's wood
+# Setting incorrect Latitude and Longitude to correct coordinates
+names <- c("St John's Wood","Ellis Wood","Tankardstown South")
+lats <- c(53.556947,53.551731081502,53.747456)
+longs <- c(-8.00471653,-9.9475836753845,-6.6196797)
+
+# Changing details for some specific woods (St. John's wood, Phoenix park wood and more)
 wronguns <- df %>% 
   filter(woodland_name %in% names) %>% 
   mutate(lat = lats, lon = longs) 
 
+borough <- df %>% 
+  filter(county %in% "Borough") %>% 
+  mutate(county = "Dublin")
+
+meath_louth <- df %>% 
+  filter(county %in% "Meath/Louth") %>% 
+  mutate(county = "Meath")
+
+cork_waterford <- df %>% 
+  filter(county %in% "Cork/Waterford") %>% 
+  mutate(county = "Cork")
+
 df_clean <- df %>% 
   filter(!woodland_name %in% names) %>% 
-  add_row(wronguns)
+  filter(!county %in% "Borough") %>% 
+  filter(!county %in% "Meath/Louth") %>% 
+  filter(!county %in% "Cork/Waterford") %>% 
+  add_row(wronguns)  %>% 
+  add_row(borough) %>% 
+  add_row(meath_louth) %>% 
+  add_row(cork_waterford)
 
-df_clean %>% filter(woodland_name %in% names) # Checking new data frame contains wood of interest
+# Checking new data frame contains wood of interest
+df_clean %>% 
+  filter(woodland_name %in% names | woodland_name %in% "Phoenix Park") 
 
 # Changing empty ownership names to Not Stated
 df_clean$ownership[df_clean$ownership==""] <- "Not Stated"
 
 
-df_clean$ownership0 <- ifelse(grepl("private",df_clean$ownership,ignore.case=T),"Private",
-                              ifelse(grepl("coillte|other state body|local authority|Bórd na Móna", df_clean$ownership,ignore.case=T),"Public",
-                              ifelse(grepl("npws",df_clean$ownership,ignore.case=T),"NPWS","Unknown")))
+df_clean$ownership0 <- ifelse(grepl("private", df_clean$ownership, ignore.case=T), "Private",
+                              ifelse(grepl("coillte|other state body|local authority|Bórd na Móna", df_clean$ownership,ignore.case=T), "Public",
+                                     ifelse(grepl("npws", df_clean$ownership, ignore.case=T), "NPWS", "Unknown")))
 
-
+# Setting provinces counties
+munster <- c("Kerry", "Limerick", "Cork", "Tipperary", "Waterford", "Clare")
+connacht <- c("Galway", "Leitrim", "Mayo", "Roscommon", "Sligo")
+leinster <- c("Kildare", "Carlow", "Kilkenny", "Laois", "Longford", "Louth", 
+              "Meath", "Offaly", "Wexford", "Wicklow", "Westmeath", "Dublin")
+ulster <- c("Monaghan", "Cavan", "Donegal")
 
 
 ui <- bootstrapPage(
@@ -39,28 +67,30 @@ ui <- bootstrapPage(
   
   
   absolutePanel(top = 10, right = 10,
-                radioButtons("owner", label = "Ownership", choices = c("All","Public","NPWS")),
-                selectInput("county", label = "County", choices = c("All Counties",unique(df_clean$county))))
+                radioButtons("owner", label = "Ownership", choices = c("All", "Public", "NPWS")),
+                selectInput("county", label = "County", choices = list("All Counties", 
+                                                                       'Connacht'=c("Mayo","Sligo","Leitrim","Galway","Rsocommon"),
+                                                                       'Leinster'= c("Carlow","Dublin","Kildare","Kilkenny","Offaly","Longford","Louth","Meath","Laois","Westmeath","Wexford","Wicklow"),
+                                                                       'Munster' = c("Clare","Cork","Kerry","Limerick","Tipperary","Waterford"),
+                                                                       'Ulster'  = c("Cavan","Donegal","Monaghan")
+                                                                       
+                )))
 )
-
-  
-  
 
 
 server <- function(input, output, session) {
   
-
-  
- 
-    
-  
   output$map <- renderLeaflet({leaflet(df_clean %>% 
-                filter(if (input$county %in% "All Counties") county %in% county 
-                       else county %in% input$county) %>% 
-                filter(if (input$owner %in% "All") ownership0 %in% ownership0
-                       else if (input$owner %in% "Public") ownership0 %in% c("Public","NPWS")
-                       else ownership0 %in% input$owner)
-    ) %>%
+                                         filter(if (input$county %in% "All Counties") county %in% county 
+                                                else if (input$county %in% "Munster") county %in% munster
+                                                else if (input$county %in% "Leinster") county %in% leinster
+                                                else if (input$county %in% "Connacht") county %in% connacht
+                                                else if (input$county %in% "Ulster") county %in% ulster
+                                                else county %in% input$county) %>% 
+                                         filter(if (input$owner %in% "All") ownership0 %in% ownership0
+                                                else if (input$owner %in% "Public") ownership0 %in% c("Public", "NPWS")
+                                                else ownership0 %in% input$owner)
+  ) %>%
       addTiles() %>%  # Add default OpenStreetMap map tiles
       addMarkers(~lon, ~lat, label = ~htmlEscape(woodland_name), 
                  popup = ~paste0("<font size=3>", '<strong>', woodland_name, '</strong>',
@@ -69,10 +99,8 @@ server <- function(input, output, session) {
                                  "<br/>Threat status: ", threat_rate, 
                                  "<br/>Area (ha): ", area, 
                                  "<br/>Ownership: ", ownership ))}
-    )
-  
+  )
   
 }
 
 shinyApp(ui, server)
-
