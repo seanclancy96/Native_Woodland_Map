5b42c50114be38dab85375d72688a2b63ce3ee81
John <johnmullane10@gmail.com>
2021-04-05 12:29:40 +0100

    Merged Sean's code with mine

--- extra.R
+++ extra.R
@@ -1,17 +1,55 @@
-library(shiny)
 library(tidyverse)
 library(leaflet)
 library(htmltools)
 
 # Reading data 
-df_clean <- readRDS(file = "df_clean.rds")
+df <- readRDS(file = "df.rds")
 
-# Setting provinces of counties
-munster <- c("Kerry", "Limerick", "Cork", "Tipperary", "Waterford", "Clare")
-connacht <- c("Galway", "Leitrim", "Mayo", "Roscommon", "Sligo")
-leinster <- c("Kildare", "Carlow", "Kilkenny", "Laois", "Longford", "Louth", 
-              "Meath", "Offaly", "Wexford", "Wicklow", "Westmeath", "Dublin")
-ulster <- c("Monaghan", "Cavan", "Donegal")
+# Setting St. Johns Wood Latitude and Longitude to correct coordinates
+johns_name <- "St John's Wood"
+lat_johns <- 53.556947
+lon_johns <- -8.00471653
+
+# Changing details for some specific woods (St. John's wood, Phoenix park wood and more)
+johns <- df %>% 
+  filter(woodland_name %in% johns_name) %>% 
+  mutate(lat = lat_johns, lon = lon_johns) 
+
+borough <- df %>% 
+  filter(county %in% "Borough") %>% 
+  mutate(county = "Dublin")
+
+meath_louth <- df %>% 
+  filter(county %in% "Meath/Louth") %>% 
+  mutate(county = "Meath")
+
+cork_waterford <- df %>% 
+  filter(county %in% "Cork/Waterford") %>% 
+  mutate(county = "Cork")
+
+df_clean <- df %>% 
+  filter(!woodland_name %in% johns_name) %>% 
+  filter(!county %in% "Borough") %>% 
+  filter(!county %in% "Meath/Louth") %>% 
+  filter(!county %in% "Cork/Waterford") %>% 
+  add_row(johns)  %>% 
+  add_row(borough) %>% 
+  add_row(meath_louth) %>% 
+  add_row(cork_waterford)
+
+# Checking new data frame contains wood of interest
+df_clean %>% 
+  filter(woodland_name %in% johns_name | woodland_name %in% "Phoenix Park") 
+
+# Changing empty ownership names to Not Stated
+df_clean$ownership[df_clean$ownership==""] <- "Not Stated"
+
+# Setting ownership variable for Private/Public/NPWS
+df_clean$ownership0 <- ifelse(grepl("private", df_clean$ownership, ignore.case=T), "Private",
+                              ifelse(grepl("coillte|other state body|local authority|Bórd na Móna", df_clean$ownership,ignore.case=T), "Public",
+                                     ifelse(grepl("npws", df_clean$ownership, ignore.case=T), "NPWS", "Unknown")))
+
+saveRDS(df_clean,"df_clean.rds")
 
 # User Interface
 ui <- bootstrapPage(
@@ -26,12 +64,12 @@ ui <- bootstrapPage(
                                                                     df_clean$county %>% 
                                                                       unique() %>% # Taking unique county vals
                                                                       sort() # Sorting counties alphabetically to improve navigation
-                                                                    ), width = '180px'))
+                ), width = '180px'))
 )
 
 # Server
 server <- function(input, output, session) {
-
+  
   output$map <- renderLeaflet({leaflet(df_clean %>% 
                                          filter(if (input$county %in% "All Counties") county %in% county 
                                                 else if (input$county %in% "Munster") county %in% munster
@@ -52,7 +90,8 @@ server <- function(input, output, session) {
                                  "<br/>Area (ha): ", area, 
                                  "<br/>Ownership: ", ownership ))}
   )
-
+  
 }
 
 shinyApp(ui, server)
+
